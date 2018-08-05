7a502764e2bc62e11204d8d8b79777184b9a694b
pamepeixinho <pameiupip@gmail.com>
2018-08-05 18:32:21 -0300

    add instructions

--- app/containers/StartClass/actions.js
+++ app/containers/StartClass/actions.js
@@ -6,10 +6,17 @@
 
 import {
   TOGGLE_ACTIVE_CLASS,
+  FINISH_CLASS,
 } from './constants';
 
 export function toggleActiveClass() {
   return {
     type: TOGGLE_ACTIVE_CLASS,
+  };
+}
+
+export function finishClass() {
+  return {
+    type: FINISH_CLASS,
   };
 }
