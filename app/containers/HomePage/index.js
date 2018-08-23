25c7da3904d2772fa1546534312686a2e13a46c4
Pamela Peixinho <pamela.peixinho@quintoandar.com.br>
2018-08-23 08:42:03 -0300

    Loading button to the first step (#15)

--- app/containers/HomePage/index.js
+++ app/containers/HomePage/index.js
@@ -5,6 +5,7 @@ import styled from 'styled-components';
 import { connect } from 'react-redux';
 import { compose } from 'recompose';
 
+import CircularProgress from '@material-ui/core/CircularProgress';
 import MuiPaper from '@material-ui/core/Paper';
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
@@ -15,7 +16,7 @@ import DefaultWrapper from 'components/DefaultWrapper';
 
 import injectReducer from '../../utils/injectReducer';
 import reducer from './reducer';
-import { selectName, selectLink } from './selectors';
+import { selectName, selectLink, selectLoading } from './selectors';
 import { updateField, goToNextStep } from './actions';
 
 const Paper = styled(MuiPaper)`
@@ -72,7 +73,7 @@ class HomePage extends React.PureComponent {
               disabled={buttonDisabled}
               style={{ marginTop: 32, position: 'absolute', bottom: 22, right: 32 }}
             >
-              Começar
+              { this.props.isLoading ? <CircularProgress size={24} /> : 'Começar' }
             </Button>
           </Paper>
         </DefaultWrapper>
@@ -92,6 +93,7 @@ HomePage.propTypes = {
 const mapStateToProps = createStructuredSelector({
   name: selectName,
   link: selectLink,
+  isLoading: selectLoading,
 });
 
 const mapDispatchToProps = (dispatch) => ({
