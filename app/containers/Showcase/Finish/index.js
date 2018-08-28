d8ef0a822c8a1e5f0173e9698cd44ab896cd1ed5
pamepeixinho <pameiupip@gmail.com>
2018-08-28 10:14:45 -0300

    first take in gif and button

--- app/containers/Showcase/Finish/index.js
+++ app/containers/Showcase/Finish/index.js
@@ -5,16 +5,12 @@
  */
 
 import React from 'react';
-import styled from 'styled-components';
+// import styled from 'styled-components';
 // import PropTypes from 'prop-types';
 
 import EmotionsGif from 'images/emotions.gif';
 
 import AppBar from 'components/AppBar';
-
-const Img = styled.img`
-  width: 90%;
-`;
 
 
 export class Finish extends React.PureComponent { // eslint-disable-line react/prefer-stateless-function
