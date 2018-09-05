3954dfe3f26a9a84860f35d19befb65beb853ee9
pamepeixinho <pameiupip@gmail.com>
2018-09-05 06:19:45 -0700

    fix size-n

--- app/components/VideoCapture/index.js
+++ app/components/VideoCapture/index.js
@@ -45,7 +45,7 @@ class VideoCapture extends React.PureComponent { // eslint-disable-line react/pr
           <Webcam
             audio={false}
             ref={this.setRef}
-            style={{ display: 'inline-block', height: 720, width: '100%' }}
+            style={{ display: 'inline-block', height: 705 }}
             screenshotFormat="image/jpeg"
             onUserMedia={this.loop}
             videoConstraints={videoConstraints}
